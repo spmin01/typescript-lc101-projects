3a65b525d80aaefd33609347f97b2e546fae4bd9
Shaun Minnihan <spminnihan@gmail.com>
2019-10-17 18:29:27 -0500

    added Astronaut, Cargo, Rocket files

---
+++ studio/Astronaut.ts
@@ -0,0 +1,11 @@
+import { Payload } from './Payload';
+
+class Astronaut implements Payload {
+    massKg: number;
+    name: string;
+    
+    constructor(massKg: number, name: string) {
+        this.massKg = masskg;
+        this.name = name;
+    }
+}
